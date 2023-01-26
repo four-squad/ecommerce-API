4ce4789d827e91040a19e69d59cd68056a58c938
unknown <techmin7@gmail.com>
2023-01-26 08:43:05 +0700

    feat: CRUD cart with fix relation

--- features/cart/handler/handler.go
+++ features/cart/handler/handler.go
@@ -53,3 +53,59 @@ func (cc *cartControll) GetByIdC() echo.HandlerFunc {
 		return c.JSON(helper.PrintSuccessReponse(http.StatusOK, "berhasil menampilkan product", ToResponse(res)))
 	}
 }
+func (cc *cartControll) GetByIdU() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		fmt.Println("======hdl1=====")
+		token := c.Get("user")
+		// idCart, errBind := strconv.Atoi(c.Param("idCart"))
+		// if errBind != nil {
+		// 	return c.JSON(helper.PrintErrorResponse("Data not found"))
+		// }
+		fmt.Println("======hdl2=====")
+		res, err2 := cc.srv.GetByIdU(token)
+		if err2 != nil {
+			return c.JSON(helper.PrintErrorResponse(err2.Error()))
+		}
+		return c.JSON(helper.PrintSuccessReponse(http.StatusOK, "berhasil menampilkan product", ToResponseArr(res)))
+	}
+}
+func (cc *cartControll) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		idCart, errBind := strconv.Atoi(c.Param("idCart"))
+		if errBind != nil {
+			return c.JSON(helper.PrintErrorResponse("Error binding data"))
+		}
+		token := c.Get("user")
+		updatedData := UptReq{}
+		if err := c.Bind(&updatedData); err != nil {
+			return c.JSON(http.StatusBadRequest, "kesalahan input")
+		}
+		//-----------
+		// Read file
+		//-----------
+		// file, err := c.FormFile("image")
+		// if err != nil {
+		// 	file = nil
+		// }
+		_, err2 := cc.srv.Update(token, uint(idCart), *ToCore(updatedData))
+		if err2 != nil {
+			return c.JSON(helper.PrintErrorResponse(err2.Error()))
+		}
+		return c.JSON(http.StatusOK, helper.SuccessResponse("Update berhasil"))
+	}
+}
+func (cc *cartControll) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		idCart, errBind := strconv.Atoi(c.Param("idCart"))
+		if errBind != nil {
+			return c.JSON(helper.PrintErrorResponse("Kesalahan input"))
+		}
+		token := c.Get("user")
+		err2 := cc.srv.Delete(token, uint(idCart))
+		if err2 != nil {
+			return c.JSON(helper.PrintErrorResponse(err2.Error()))
+		}
+		return c.JSON(http.StatusOK, helper.SuccessResponse("Delete product berhasil"))
+	}
+}
--- features/cart/services/service.go
+++ features/cart/services/service.go
@@ -54,3 +54,51 @@ func (cuu *cartUseCase) GetByIdC(token interface{}, idCart uint) (cart.CoreCart,
 
 	return res, nil
 }
+func (cuu *cartUseCase) GetByIdU(token interface{}) ([]cart.CoreCart, error) {
+	fmt.Println("======srv1=====")
+	idUser := helper.ExtractToken(token)
+	res, err := cuu.qry.GetByIdU(uint(idUser))
+	fmt.Println("======srv2=====")
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "data not found"
+		} else {
+			msg = "There is a problem with the server"
+		}
+		return []cart.CoreCart{}, errors.New(msg)
+	}
+
+	return res, nil
+}
+func (cuu *cartUseCase) Update(token interface{}, idCart uint, updatedData cart.CoreCart) (cart.CoreCart, error) {
+	idUser := helper.ExtractToken(token)
+
+	res, err := cuu.qry.Update(uint(idUser), idCart, updatedData)
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "Please input correctly"
+		} else {
+			msg = "There is a problem with the server"
+		}
+		return cart.CoreCart{}, errors.New(msg)
+	}
+
+	return res, nil
+}
+func (cuu *cartUseCase) Delete(token interface{}, idCart uint) error {
+	idUser := helper.ExtractToken(token)
+	err := cuu.qry.Delete(uint(idUser), idCart)
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "Please input correctly"
+		} else {
+			msg = "There is a problem with the server"
+		}
+		return errors.New(msg)
+	}
+
+	return nil
+}
